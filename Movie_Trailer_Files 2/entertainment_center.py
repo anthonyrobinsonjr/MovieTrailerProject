7fe275a61532c76acbb99e0e52448d61be83620e
Dr. Anthony Robinson <arobinson@inspired2g.com>
2016-07-04 13:22:56 -0400

    Update entertainment_center.py

--- Movie_Trailer_Files 2/entertainment_center.py
+++ Movie_Trailer_Files 2/entertainment_center.py
@@ -1,7 +1,7 @@
 import fresh_tomatoes
 import media
 
-Toy Story = media.Movie("Toy Story",
+Toy_Story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=27EzuAobbi0")
@@ -11,32 +11,32 @@ Avatar = media.Movie("Avatar",
                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
 
-Teenage Mutant Ninja Turtles = media.Movie("tmnt",
+Teenage_Mutant_Ninja_Turtles = media.Movie("tmnt",
                    "The turtles must confront an even greater nemisis",
                    "http://www.impawards.com/2014/posters/teenage_mutant_ninja_turtles_ver17.jpg",
                     "https://www.youtube.com/watch?v=HeaugHGd1Kw")
 
 #tmnt.show_trailer()
 
-Lion King = media.Movie("lion_king",
+Lion_King = media.Movie("lion_king",
                    "The adventures of a young lion who must thwart his \
                    uncle's attempts to take over the pride land",
                    "https://upload.wikimedia.org/wikipedia/en/3/3d/The_Lion_King_poster.jpg",
                     "https://www.youtube.com/watch?v=4sj1MT05lAA")
 
-Pursuit of Happyness = media.Movie("pursuit_of_happyness",
+Pursuit_of_Happyness = media.Movie("pursuit_of_happyness",
                    "A biographical film of one man's struggle and perseverance",
                    "http://www.gstatic.com/tv/thumb/movieposters/162523/p162523_p_v8_ad.jpg",
                     "https://www.youtube.com/watch?v=89Kq8SDyvfg")
 
-planet_of_apes = media.Movie("planet_of_apes",
+Planet_of_Apes = media.Movie("planet_of_apes",
                    "An astronaut crew crash lands on a planet in the distant \
                    future where intelligent talking apes are the dominant \
                    species, and humans are the oppressed and enslaved",
                    "http://stuffpoint.com/soundtracks/image/276594-soundtracks-rise-of-the-planet-of-the-apes-soundtrack-cover.jpg",
                     "https://www.youtube.com/watch?v=f8D2NIGEJW8")
 
-Independence Day = media.Movie("independence_day",
+Independence_Day = media.Movie("independence_day",
                    "The aliens are coming and their goal is to invade and \
                    destroy Earth. Fighting superior technology, mankind's \
                    best weapon is the will to survive",
